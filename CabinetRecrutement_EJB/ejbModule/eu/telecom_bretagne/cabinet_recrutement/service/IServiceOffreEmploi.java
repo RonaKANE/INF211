5176119845e8bfdb81392aad164608acc1584ae3
lkchao78 <limkevin.chao@telecom-bretagne.eu>
2016-10-24 16:22:56 +0200

    done

--- CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceOffreEmploi.java
+++ CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceOffreEmploi.java
@@ -1,8 +1,65 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import com.sun.tools.ws.wsdl.document.Message;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.model.Niveauqualification;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.Offreemploi;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.Secteuractivite;
 
 @Remote
 public interface IServiceOffreEmploi {
 
+	//-----------------------------------------------------------------------------
+		/**
+		 * 
+		 * @param 
+		 * @return 
+		 */
+		//-----------------------------------------------------------------------------
+		public Offreemploi getOffreEmploi(int id);
+		/**
+		 * 
+		 * @return 
+		 */
+		public List<Offreemploi> listOffreemploi();
+		//-----------------------------------------------------------------------------
+		/**
+		 * 
+		 * @return
+		 */
+		public Offreemploi updateOffreemploi(int id, String titre, String descriptif, String profil, Niveauqualification niveau, List<Secteuractivite> secteur);
+		
+		//-----------------------------------------------------------------------------
+		/**
+		 * 
+		 * @return
+		 */
+		public boolean removeOffreEmploi(int id);
+		
+		//----------------------------------------------------------------------------
+		/**
+		 * 
+		 * @return
+		 */
+		public Offreemploi newOffreEmploi(String titre, String descriptif, String profil, Niveauqualification niveau, 
+				List <Secteuractivite> secteur, Date date);
+		
+		//----------------------------------------------------------------------------
+		/**
+		 * 
+		 * @return
+		 */
+		public List<Offreemploi>  offreByEntrepriseList(int id);
+		
+		//----------------------------------------------------------------------------
+		/**
+		 * 
+		 * @return
+		 */
+		public List<Message> offreByCandidatureList();
 }
